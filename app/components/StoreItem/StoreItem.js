3a7697518e043f1b28a9d9420e617286768f1cfa
eugeniosp3 <eugeniosp3@gmail.com>
2023-10-05 13:46:13 -0400

    fixed spacing

--- app/components/StoreItem/StoreItem.js
+++ app/components/StoreItem/StoreItem.js
@@ -4,7 +4,7 @@ import React from 'react'
 function StoreItem({image, title, price}) {
   return (
     <div className="
-    xs:max-md:h-96 md:h-80 xs:max-md:gap-4 md:gap-6 lg:h-72
+    xs:max-md:h-96 md:h-80 xs:max-md:gap-4 md:gap-6 lg:h-76
     w-2/3  flex flex-col items-center justify-evenly md:justify-between ">
         <div className="flex items-center justify-center">
           <Image src={image} width={120} height={120} className="
@@ -18,7 +18,7 @@ function StoreItem({image, title, price}) {
             <p>{title}</p>
             <p className="font-semibold">${price}</p>
           </div>
-          <div className="flex items-center justify-evenly w-24 min-h-7 bg-yellow-400 rounded-lg shadow-md">
+          <div className="flex items-center justify-evenly w-24 min-h-7 bg-yellow-400 hover:bg-yellow-600 rounded-lg shadow-md cursor-pointer">
               <p className="text-sm text-gray-50 font-semibold">Add to Cart</p>
           </div>
         </div>
